7372b18f045dc919fe40a2b7d992e0731d672230
Michael <realq86@gmail.com>
2017-08-24 19:20:43 -0700

    Initial Commit

---
+++ TableViewProgrammaticallySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewProgrammaticallySwift
+//
+//  Created by Chi Hwa Michael Ting on 8/24/17.
+//  Copyright © 2017 Chi Hwa Michael Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TableViewProgrammaticallySwiftTests/TableViewProgrammaticallySwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewProgrammaticallySwiftTests.swift
+//  TableViewProgrammaticallySwiftTests
+//
+//  Created by Chi Hwa Michael Ting on 8/24/17.
+//  Copyright © 2017 Chi Hwa Michael Ting. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewProgrammaticallySwift
+
+class TableViewProgrammaticallySwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
